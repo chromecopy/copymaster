73be95b8b5f50c19a83f0869b2e646b241b8a6f5
Elias Skogevall <elias.skogevall@gmail.com>
2019-03-30 15:59:38 +0100

    setup

---
+++ background.ts
@@ -0,0 +1,12 @@
+/// <reference path="@types/chrome/index.d.ts"/>
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+				pageUrl: {},
+			})],
+			actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
+	});
+});
